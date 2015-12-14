fd2d2e0c923818632469b5d93b648cff12d06c9e
maximeberthelot <hello@maximeberthelot.fr>
2015-12-14 17:56:43 +0100

    Initial Commit

---
+++ Wandr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wandr
+//
+//  Created by Maxime Berthelot on 14/12/15.
+//  Copyright © 2015 Maxime Berthelot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
